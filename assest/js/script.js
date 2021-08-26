011eafda2c03caa9de1f5f589f1638c0a3386323
MiladSaeedi <milad138001@gmail.com>
2021-08-25 17:21:18 -0700

    edit

--- assest/js/script.js
+++ assest/js/script.js
@@ -3,3 +3,34 @@ window.addEventListener("load" , ()=>{
     loader.classList.add("hidden")
     console.log(",ilad")
 })
+
+/*owl-carsoul*/
+$('.owl-projects').owlCarousel({
+
+    ltr: true,
+    dots:true,
+    margin: 7,
+    autoplayTimeout: 2000,
+    autoplaySpeed: 1200,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+
+        786: {
+            items: 2
+        },
+        992: {
+            items: 3,
+        }
+    }
+});
+
+let loading = document.querySelector(".loading")
+function funcLoading() {
+    loading.style.dispaly = "none"
+}
+
+funcLoading()
